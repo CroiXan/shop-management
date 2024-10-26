bad0eca02e32c6b85d3ad2b04bd09841c4340119
CroiXan <xan.mark.vi@gmail.com>
2024-10-26 18:20:05 -0300

    base de ordenes

---
+++ shop/src/main/java/com/croix/shop/model/Orderitem.java
@@ -0,0 +1,5 @@
+package com.croix.shop.model;
+
+public class Orderitem {
+
+}
---
+++ shop/src/test/java/com/croix/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.croix.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
